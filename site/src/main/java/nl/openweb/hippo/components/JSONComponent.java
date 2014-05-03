aa6b8eedfd2b02f35bb3d8a8920e89e445e6249c
Ebrahim Aharpour <ebrahim.aharpour@gmail.com>
2014-05-03 22:59:29 +0200

    basic setup part 1

---
+++ site/src/main/java/nl/openweb/hippo/components/JSONComponent.java
@@ -0,0 +1,15 @@
+package nl.openweb.hippo.components;
+
+import org.hippoecm.hst.component.support.bean.BaseHstComponent;
+import org.hippoecm.hst.core.component.HstComponentException;
+import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.component.HstResponse;
+
+public class JSONComponent extends BaseHstComponent {
+
+	@Override
+	public void doBeforeRender(HstRequest request, HstResponse response)
+			throws HstComponentException {
+		
+	}
+}
