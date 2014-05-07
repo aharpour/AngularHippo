4fff24d9856f0386cc0df5a0ecbdde7bf2bd7430
Levente Bokor <levente.bokor@smile-benelux.com>
2014-05-07 21:58:04 +0200

    Routing updated to be one statement

--- site/src/main/webapp/js/app.js
+++ site/src/main/webapp/js/app.js
@@ -2,20 +2,12 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp',
-		[ 'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers' ]).config(
+		[ 'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ngRoute' ]).config(
 		[ '$routeProvider', function($routeProvider) {
 			$routeProvider.when('#/common/homepage', {
 				templateUrl : 'templates/blank.html'
 			});
-			$routeProvider.when('/:sub1', {
-				controller : 'ArticleCtrl',
-				template : '<article ng-include="templateUrl"></article>'
-			});
-			$routeProvider.when('/:sub1/:sub2', {
-				controller : 'ArticleCtrl',
-				template : '<article ng-include="templateUrl"></article>'
-			});
-			$routeProvider.when('/:sub1/:sub2/:sub3', {
+			$routeProvider.when('/:sub1?/:sub2?/:sub3?/:sub4?/:sub5?', {
 				controller : 'ArticleCtrl',
 				template : '<article ng-include="templateUrl"></article>'
 			});
